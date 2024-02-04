82db80302a70b9e77f1ab896064cf5dd201333d1
Rodrigo Vila PC <202001302h@utea.edu.pe>
2024-02-03 22:51:56 -0500

    Actualizacion de las tablas matricula y factura

--- BD_MATRICULA.sql
+++ BD_MATRICULA.sql
@@ -4,25 +4,33 @@ USE BD_MATRICULA
 
 CREATE TABLE ESTUDIANTES(
 id_estudiante INT PRIMARY KEY,
-dni VARCHAR(8) NOT NULL,
-nombre VARCHAR(8) NOT NULL,
+dni VARCHAR(8)NOT NULL,
+nombre VARCHAR(8)NOT NULL,
 codigo char(10)not null,
 )
 
 CREATE TABLE PENSIONES(
 id_PENSIONES INT PRIMARY KEY,
-monto DECIMAL(10, 2),
-fecha_pago DATE,
-estado_pago VARCHAR(20),
-id_estudiante INT,
+monto DECIMAL(10, 2) not null,
+fecha_pago DATE not null,
+estado_pago VARCHAR(20) not null,
+id_estudiante INT not null,
 FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTES(id_estudiante)
-);
+)
 
 
 CREATE TABLE MATRICULA(
-id_matricula INT PRIMARY KEY
+id_matricula INT PRIMARY KEY not null,
+fecha_matricula DATE not null,
+periodo_academico VARCHAR(20)not null,
+id_estudiante INT not null,
+FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTES(id_estudiante)
 )
 
 CREATE TABLE FACTURA(
-id_factura INT PRIMARY KEY
+id_factura INT PRIMARY KEY not null,
+fecha_emision DATE not null,
+monto_total DECIMAL(10, 2) not null,
+id_estudiante INT not null,
+FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTES(id_estudiante)
 )
