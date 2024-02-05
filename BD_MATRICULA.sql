156b9dc6270a49704899b9cac1c1d529589551d4
Benjamin Ccolque <202013261i@utea.edu.pe>
2024-02-05 16:03:38 -0500

    Modificación en la base de datos

--- BD_MATRICULA.sql
+++ BD_MATRICULA.sql
@@ -3,37 +3,40 @@ create DATABASE BD_MATRICULA
 USE BD_MATRICULA
 
 CREATE TABLE ESTUDIANTES(
-id_estudiante INT PRIMARY KEY not null,
-dni VARCHAR(8) NOT NULL,
-nombre VARCHAR(40) NOT NULL,
-apellidos varchar(40) not null,
-codigoEstudiante char(10)not null,
+id_estudiante int primary key not null,
+dni				VARCHAR(8) UNIQUE,
+codigo			VARCHAR(10) UNIQUE,
+nom_completo	VARCHAR(100) NOT NULL,
+genero			VARCHAR(10) NOT NULL,
+celular			VARCHAR(9) NOT NULL,
+correo			VARCHAR(100) NOT NULL,
+direccion		VARCHAR(100) NOT NULL
 )
 
 create TABLE PENSIONES(
-id_PENSIONES INT PRIMARY KEY,
-precioPension money not null,
-id_matricula int not null,
-foreign key (id_matricula) references matricula(id_matricula)
+id_pensiones	INT PRIMARY KEY NOT NULL,
+monto			MONEY NOT NULL,
+f_vencimiento	DATE NOT NULL,
 )
 
 CREATE TABLE MATRICULA(
-id_matricula INT PRIMARY KEY,
-precioMatricula money not null,
-semestreMatricula int not null,
-id_estudiante int not null,
-foreign key (id_estudiante) references ESTUDIANTES(id_estudiante)
+id_matricula	INT PRIMARY KEY,
+dni				VARCHAR(8) NOT NULL,
+fecha_matricula DATE NOT NULL,
+semestre		VARCHAR(6) NOT NULL,
 )
 
 create TABLE FACTURA
-(id_factura INT PRIMARY KEY,
-nombreCompleto VARCHAR(100) NOT NULL,
-codigoEstudiante char(10)not null,
-dni VARCHAR(8) NOT NULL,
-fechaEmision date not null,
-precioTotal money not null,
-tipoPago varchar(50) not null,
-id_pensiones int not null,
-foreign key (id_pensiones) references pensiones(id_pensiones)
+id_factura      INT PRIMARY KEY,
+dni             VARCHAR(8) NOT NULL,
+id_pensiones    INT NOT NULL,
+id_matricula    INT NOT NULL,
+monto_t         MONEY NOT NULL,
+fecha_emision   DATE NOT NULL,
+estado          DATE NOT NULL,
+
+FOREIGN KEY (dni) REFERENCES ESTUDIANTE(dni),
+FOREIGN KEY (id_pensiones) REFERENCES PENSIONES(id_pensiones),
+FOREIGN KEY (id_matricula) REFERENCES MATRICULA(id_matricula)
 )
 
