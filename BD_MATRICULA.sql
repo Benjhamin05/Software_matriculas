545583358915b7cfb8b29526dd6db94e2c778b52
Benjamin Ccolque <202013261i@utea.edu.pe>
2024-02-08 22:17:20 -0500

    Base de datos modificada, y eliminación de un script

--- BD_MATRICULA.sql
+++ BD_MATRICULA.sql
@@ -3,52 +3,42 @@ create DATABASE BD_MATRICULA
 USE BD_MATRICULA
 
 CREATE TABLE ESTUDIANTES(
-id_estudiante int primary key not null,
-dni				VARCHAR(8) UNIQUE,
-codigo			VARCHAR(10) UNIQUE,
-nom_completo	VARCHAR(100) NOT NULL,
-genero			VARCHAR(10) NOT NULL,
-celular			VARCHAR(9) NOT NULL,
-correo			VARCHAR(100) NOT NULL,
-direccion		VARCHAR(100) NOT NULL
+id_estudiante	INT PRIMARY KEY,
+dni				VARCHAR(8) NOT NULL,
+nombres			VARCHAR(50) NOT NULL,
+apellidos		VARCHAR(50) NOT NULL,
+codigo			VARCHAR(10) NOT NULL,
+correo			VARCHAR(100) NOT NULL
 )
 
-<<<<<<< HEAD
 CREATE TABLE PENSIONES(
-id_PENSIONES INT PRIMARY KEY
-id_estudiante INT 
-)
-
-CREATE TABLE MATRICULA(
-id_matricula INT PRIMARY KEY
-id_estudiante INT 
-=======
-create TABLE PENSIONES(
-id_pensiones	INT PRIMARY KEY NOT NULL,
+id_pensiones	INT PRIMARY KEY,
+id_estudiante	INT NOT NULL,
+n_cuota			INT NOT NULL,
 monto			MONEY NOT NULL,
-f_vencimiento	DATE NOT NULL,
-fecha_p			INT NOT NULL,
+fecha_vencimiento DATE NOT NULL,
+mora			MONEY NOT NULL,
+
+FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTES(id_estudiante)
 )
 
 CREATE TABLE MATRICULA(
 id_matricula	INT PRIMARY KEY,
-dni				VARCHAR(8) NOT NULL,
+id_estudiante	INT NOT NULL,
+carrera			VARCHAR(100) NOT NULL,
 fecha_matricula DATE NOT NULL,
-semestre		VARCHAR(6) NOT NULL,
+semestre		INT NOT NULL,
+monto			MONEY NOT NULL,
+
+FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTES(id_estudiante)
 )
 
-create TABLE FACTURA
-id_factura      INT PRIMARY KEY,
-dni             VARCHAR(8) NOT NULL,
-id_pensiones    INT NOT NULL,
-id_matricula    INT NOT NULL,
-monto_t         MONEY NOT NULL,
-fecha_emision   DATE NOT NULL,
-estado          DATE NOT NULL,
+CREATE TABLE FACTURA(
+id_factura		INT PRIMARY KEY NOT NULL,
+id_estudiante	INT NOT NULL,
+fecha_emision	DATE NOT NULL,
+mora			MONEY NOT NULL,
+monto_total		MONEY NOT NULL,
 
-FOREIGN KEY (dni) REFERENCES ESTUDIANTE(dni),
-FOREIGN KEY (id_pensiones) REFERENCES PENSIONES(id_pensiones),
-FOREIGN KEY (id_matricula) REFERENCES MATRICULA(id_matricula)
->>>>>>> 156b9dc6270a49704899b9cac1c1d529589551d4
+FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTES(id_estudiante)
 )
-
