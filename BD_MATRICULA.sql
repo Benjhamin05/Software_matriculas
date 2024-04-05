e4eaa2a21127bf9fc6045fb61840ee7423489819
Benjamin Ccolque Rojas <202013261i@utea.edu.pe>
2024-04-05 10:00:47 -0500

    Cambio de base de datos

--- BD_MATRICULA.sql
+++ BD_MATRICULA.sql
@@ -2,43 +2,31 @@ CREATE DATABASE BD_MATRICULA
 
 USE BD_MATRICULA
 
-CREATE TABLE ESTUDIANTES(
+CREATE TABLE ESTUDIANTE(
 id_estudiante	INT PRIMARY KEY,
 dni				VARCHAR(8) NOT NULL,
 nombres			VARCHAR(50) NOT NULL,
 apellidos		VARCHAR(50) NOT NULL,
-codigo			VARCHAR(10) NOT NULL,
-correo			VARCHAR(100) NOT NULL
-)
-
-CREATE TABLE PENSIONES(
-id_pensiones		INT PRIMARY KEY,
-id_estudiante		INT NOT NULL,
-n_cuota				INT NOT NULL,
-monto				MONEY NOT NULL,
-fecha_vencimiento	DATE NOT NULL,
-mora				MONEY NOT NULL,
-
-FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTES(id_estudiante)
+telefono		VARCHAR(9) NOT NULL
 )
 
 CREATE TABLE MATRICULA(
-id_matricula	INT PRIMARY KEY,
-id_estudiante	INT NOT NULL,
-carrera			VARCHAR(100) NOT NULL,
-fecha_matricula DATE NOT NULL,
-semestre		INT NOT NULL,
-monto			MONEY NOT NULL,
+id_matricula		INT PRIMARY KEY,
+id_estudiante		INT NOT NULL,
+nivel				VARCHAR(50) NOT NULL,
+monto_matricula		MONEY NOT NULL,
+fecha_m				DATE NOT NULL,
 
-FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTES(id_estudiante)
+FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTE(id_estudiante)
 )
 
-CREATE TABLE FACTURA(
-id_factura		INT PRIMARY KEY NOT NULL,
-id_estudiante	INT NOT NULL,
-fecha_emision	DATE NOT NULL,
-mora			MONEY NOT NULL,
-monto_total		MONEY NOT NULL,
+CREATE TABLE PENSION(
+id_pensiones		INT PRIMARY KEY,
+id_estudiante		INT NOT NULL,
+n_pension			INT NOT NULL,
+monto				MONEY NOT NULL,
+fecha_p				DATE NOT NULL,
+mora				MONEY NOT NULL,
 
-FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTES(id_estudiante)
+FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTE(id_estudiante)
 )
